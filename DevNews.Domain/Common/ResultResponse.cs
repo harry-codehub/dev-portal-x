05d130a9ddabcb2982f49d204d914618be7fe259
Fernando R <11729082+harry-codehub@users.noreply.github.com>
2026-01-08 15:31:14 +0100

    init

---
+++ DevNews.Domain/Common/ResultResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace DevNews.Domain.Common;
+public class ResultResponse<T>(bool isSuccess, T data, string errorMessage)
+{
+    private bool IsSuccess { get; set; } = isSuccess;
+    public T Data { get; private set; } = data;
+    public string ErrorMessage { get; private set; } = errorMessage;
+
+    public static ResultResponse<T> Success(T data)
+    {
+        return new ResultResponse<T>(true, data, null);
+    }
+
+    public static ResultResponse<T> Failure(string errorMessage)
+    {
+        return new ResultResponse<T>(false, default(T), errorMessage);
+    }
+
+    public override string ToString()
+    {
+        return IsSuccess ? $"Success: {Data}" : $"Failure: {ErrorMessage}";
+    }
+}
